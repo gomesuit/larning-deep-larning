97027714b5c17511ea2540c03ac0b5e89eab141a
gomesuit <gomesuit@gmail.com>
2017-09-04 03:08:05 +0900

    comment

--- 3.4/ten.py
+++ 3.4/ten.py
@@ -5,14 +5,18 @@ import tensorflow as tf
 # モデル設定
 tf.set_random_seed(0)
 
+# 重み
 w = tf.Variable(tf.zeros([2, 1]))
+# バイアス
 b = tf.Variable(tf.zeros([1]))
 
 x = tf.placeholder(tf.float32, shape=[None, 2])
 t = tf.placeholder(tf.float32, shape=[None, 1])
 y = tf.nn.sigmoid(tf.matmul(x, w) + b)
 
+# 交差エントロピー誤差関数
 cross_entropy = - tf.reduce_sum(t * tf.log(y) + (1 - t) * tf.log(1 - y))
+# 確率的勾配降下法
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.to_float(tf.greater(y, 0.5)), t)
